fb2693091456dae1db21b5ba217cf81b68443eb4
SajanHirusha <164228734+SajanHirusha@users.noreply.github.com>
2024-03-20 23:43:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll',()=>{
+    if(window.pageYOffset>10){
+        document.getElementById('nav').style.cssText="background-color :#23262a; box-shadow:5px 5px 3px 3px #24272b";
+
+    }
+    else{
+        document.getElementById('nav').style.cssText="background-color :black; box-shadow:0 ";
+    }
+});
